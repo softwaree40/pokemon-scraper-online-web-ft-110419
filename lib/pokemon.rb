e307d557b36a383ab59084ac39ce30bf0e8f69db
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 21:08:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,9 +10,7 @@ class Pokemon
       
     end
     def save
-      sql = <<-SQL 
-           SQL
-      DB[:conn].execute("INSERT INTO pokemon (name,type,db) VALUES ("?","?","?")",self.name,self.type,self.db)
+      DB[:conn].execute(<<-SQL INSERT INTO pokemon (name,type,db) VALUES ("?","?","?")self.name,self.type,self.db)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM Pokemon")[0][0]
      end
 end
