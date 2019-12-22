61d8e20daa39ef691554d535000a0ab830810afb
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-22 21:24:34 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -10,7 +10,7 @@ class Pokemon
       
     end
     def self.save(name,type,db)
-       sql = <<-SQL INSERT INTO pokemon (name,type,db)
+       sql = <<-SQL INSERT INTO pokemon (name,type)
         
     end
       
